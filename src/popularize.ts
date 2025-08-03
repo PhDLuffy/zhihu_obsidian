31e888017e8f4c73e817ad207a53fa5d99839f55
PhDLuffy <PhDLuffy@phdluffy.com>
2025-08-03 18:41:26 +0800

    去除推广脚注
    如果文章有参考文献链接，推广脚注会出现在参考文献链接之上，影响观感。

--- src/popularize.ts
+++ src/popularize.ts
@@ -1,10 +1,5 @@
 export function addPopularizeStr(content: string) {
     return (
-        content +
-        '<blockquote>\
-<p>本文由 <a href="https://zhuanlan.zhihu.com/p/1901622331102696374">\
-Zhihu on Obsidian\
-</a> 创作并发布</p>\
-</blockquote>'
+        content
     );
 }
